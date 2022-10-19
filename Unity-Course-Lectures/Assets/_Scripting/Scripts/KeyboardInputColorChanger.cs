e54df19c1f726319265c4f9b14b43405b92a12db
Alebrex99 <ale.brex99@gmail.com>
2022-10-19 12:50:05 +0200

    prova github

--- Unity-Course-Lectures/Assets/_Scripting/Scripts/KeyboardInputColorChanger.cs
+++ Unity-Course-Lectures/Assets/_Scripting/Scripts/KeyboardInputColorChanger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class KeyboardInputColorChanger : MonoBehaviour
 {
-    //ciao
+    //COMMENTO DI PROVA GIT HUB
     private Color[] _colors = new[]
         {Color.red, Color.blue, Color.green, Color.magenta, Color.white, Color.cyan, Color.yellow, Color.black};
 
